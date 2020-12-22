a799898e1fa3cffe01c918de4c1c987d444009b7
Phan Dung Tri <phandungtri99@gmail.com>
2020-12-22 19:10:21 +0700

    new: heal effect

--- server/src/entities/effects/createEffect.ts
+++ server/src/entities/effects/createEffect.ts
@@ -2,6 +2,7 @@ import EFFECT_NAME from "../../../../shared/src/effectName";
 import Game from "../Game";
 import Player from "../Player";
 import Effect from "./Effect";
+import HealEffect from "./HealEffect";
 import PunchEffect from "./PunchEffect";
 import VoidEffect from "./VoidEffect";
 
@@ -9,6 +10,8 @@ const createEffect = (name: EFFECT_NAME, game: Game, source: Player): Effect => 
   switch (name) {
     case EFFECT_NAME.Punch:
       return new PunchEffect(game, source);
+    case EFFECT_NAME.Heal:
+      return new HealEffect(game, source);
     default:
       return new VoidEffect(game, source);
   }
