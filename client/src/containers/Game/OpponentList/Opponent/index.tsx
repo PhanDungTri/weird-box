a3cdd36f472980b7e109d7e5f5c7675d58304939
Phan Dung Tri <phandungtri99@gmail>
2020-12-29 12:59:20 +0700

    add: poison animation

--- client/src/containers/Game/OpponentList/Opponent/index.tsx
+++ client/src/containers/Game/OpponentList/Opponent/index.tsx
@@ -6,6 +6,7 @@ import Sprite from "../../../../components/Sprite";
 import useSocketEvent from "../../../../hooks/useSocketEvent";
 import PunchEffectAnimation from "../../../../assets/sprites/punch_animation.png";
 import HealEffectAnimation from "../../../../assets/sprites/heal_animation.png";
+import PoisonEffectAnimation from "../../../../assets/sprites/poison_animation.png";
 
 interface IPlayer {
   id: string;
@@ -24,7 +25,7 @@ const setEffectAnimation = (effect: EFFECT_NAME): JSX.Element => {
         <Sprite
           key={EFFECT_NAME.Punch}
           step={7}
-          tick={2}
+          tick={3}
           src={PunchEffectAnimation}
           repeat={0}
           size={[62, 46]}
@@ -37,8 +38,21 @@ const setEffectAnimation = (effect: EFFECT_NAME): JSX.Element => {
         <Sprite
           key={EFFECT_NAME.Heal}
           step={8}
-          tick={2}
+          tick={3}
           src={HealEffectAnimation}
+          repeat={0}
+          size={[62, 46]}
+          scale={2}
+          centerize
+        />
+      );
+    case EFFECT_NAME.Poison:
+      return (
+        <Sprite
+          key={EFFECT_NAME.Poison}
+          step={11}
+          tick={3}
+          src={PoisonEffectAnimation}
           repeat={0}
           size={[62, 46]}
           scale={2}
@@ -56,7 +70,7 @@ const Opponent = (props: IPlayer): JSX.Element => {
   useSocketEvent(SOCKET_EVENT.TakeEffect, (data: IEffectRes) => {
     if (data.id === props.id) {
       setEffect(data.effect);
-      setTimeout(() => setEffect(EFFECT_NAME.Void), 400);
+      setTimeout(() => setEffect(EFFECT_NAME.Void), 500);
     }
   });
 
