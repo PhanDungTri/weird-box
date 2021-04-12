83c50f6440a0049fde78aef4f203bb763d982df9
Phan Dung Tri <phandungtri99@gmail.com>
2021-04-12 19:05:15 +0700

    wip

--- client/views/Game/Opponents/Opponent.tsx
+++ client/views/Game/Opponents/Opponent.tsx
@@ -1,20 +1,30 @@
-import { memo, useCallback } from "react";
-import { PlayerInfo } from "../../../../shared/@types";
+import { memo, useCallback, useEffect, useState } from "react";
+import { PlayerInfo, SERVER_EVENT_NAME } from "../../../../shared/@types";
 import useCommonPlayerState from "../../../hooks/useCommonPlayerState";
 import { useGameState } from "../../../hooks/useStore";
+import socket from "../../../services/socket";
 import { disabledStyle } from "../../../styles";
 import SpellAnimation from "../SpellAnimation";
 import Status from "../Status";
 import { opponentNameStyle, opponentStyle } from "./styles";
 
 const Opponent = ({ id, name }: PlayerInfo): JSX.Element => {
-  const { isEliminated, triggeredSpell, resetTriggeredSpell } = useCommonPlayerState(id);
+  const [isEliminated, eliminate] = useState(false);
+
+  useEffect(() => {
+    const onEliminatePlayer = (target: string) => {
+      if (target === id) eliminate(true);
+    };
+
+    socket.on(SERVER_EVENT_NAME.PlayerEliminated, onEliminatePlayer);
+    return () => void socket.off(SERVER_EVENT_NAME.PlayerEliminated, onEliminatePlayer);
+  }, []);
 
   return (
     <div css={[opponentStyle, isEliminated && disabledStyle]}>
       <Status id={id} />
       <div css={opponentNameStyle}>{name}</div>
-      <SpellAnimation spell={triggeredSpell} onAnimationEnd={resetTriggeredSpell} />
+      <SpellAnimation id={id} />
     </div>
   );
 };
