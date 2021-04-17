ca1e3c4e1bd78ac4123bbfb3fbdb340e74c38b2e
Phan Dung Tri <phandungtri99@gmail.com>
2021-04-17 11:00:36 +0700

    add: trigger when reach frame

--- client/components/SpriteSheet/index.tsx
+++ client/components/SpriteSheet/index.tsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useEffect } from "react";
 import Sprite, { SpriteProps } from "../Sprite";
 import { spriteSheetStyle } from "./styles";
 
@@ -6,9 +6,28 @@ type SpriteSheetProps = SpriteProps & {
   steps: number;
   fps?: number;
   loop?: boolean;
+  onReachFrame?: Record<number, () => void>;
 };
 
-const SpriteSheet = ({ steps, fps = 12, loop = false, ...spriteProps }: SpriteSheetProps): JSX.Element => {
+const SpriteSheet = ({
+  steps,
+  fps = 12,
+  loop = false,
+  onReachFrame,
+  ...spriteProps
+}: SpriteSheetProps): JSX.Element => {
+  useEffect(() => {
+    const timeouts: number[] = [];
+    const frameTime = fps / steps;
+
+    for (const key in onReachFrame) {
+      const frame = parseInt(key);
+      if (frame > 0 && frame <= steps) timeouts.push(window.setTimeout(onReachFrame[frame], frameTime * (frame - 1)));
+    }
+
+    return () => timeouts.forEach((t) => clearTimeout(t));
+  }, []);
+
   return (
     <Sprite
       css={spriteSheetStyle(steps, fps, loop, spriteProps.size[0] * (spriteProps.scale || 1) * steps)}
